4d5b11d7521c4401ff3baad74750e9e1861dce60
lucys-s <lucyjqsun@gmail.com>
2023-09-29 00:07:28 +0800

    Add aboutUs page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import MessageStandalone from './MessageStandalone'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import AboutUs from './AboutUs'
 
 const App = props => {
   return (
@@ -18,6 +19,9 @@ const App = props => {
 
             {/* a route to see a list of all messages */}
             <Route path="/messages" element={<Messages />} />
+
+            {/* a route to see a list of all messages */}
+            <Route path="/aboutus" element={<AboutUs />} />
 
             {/* a route for just a single message, where the id of the desired message is passed as a parameter */}
             <Route
